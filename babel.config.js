bf6076d400613943ea99da51f0b2ddaa6d0844da
Ghost <ghxstofsolana@gmail.com>
2025-07-24 21:06:32 +0200

    added babe.config.js

---
+++ babel.config.js
@@ -0,0 +1,29 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@': './src',
+          },
+          extensions: [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.android.js',
+            '.android.tsx',
+            '.ios.js',
+            '.ios.tsx',
+            '.json',
+          ],
+        },
+      ],
+      // Add other plugins here, e.g., 'react-native-reanimated/plugin' (should be last)
+    ],
+  };
+}; 
